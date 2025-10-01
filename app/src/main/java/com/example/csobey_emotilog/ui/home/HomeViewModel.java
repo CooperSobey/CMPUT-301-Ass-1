11385af7d35cdb68fb2581e3e9fbfd9bf13f5321
CooperSobey <csobey@ualberta.ca>
2025-10-01 16:37:21 -0600

    Added super duper barebone comments but Im on a time crunch

--- app/src/main/java/com/example/csobey_emotilog/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/csobey_emotilog/ui/home/HomeViewModel.java
@@ -4,6 +4,8 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+//Honestly this came with the preset but I never use it and not 100% sure if I need it but it
+// breaks without it
 public class HomeViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
